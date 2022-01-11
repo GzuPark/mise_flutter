a59dd3332a927975fa359f64fcfb4693cc3c65c8
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-11 22:30:28 +0900

    Apply main page design

--- lib/main.dart
+++ lib/main.dart
@@ -29,20 +29,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Color> colors = [
+    const Color(0xFF0077c2),
+    const Color(0xFF009ba9),
+    const Color(0xFFfe6300),
+    const Color(0xFFd80019),
+  ];
+
+  List<String> icon = [
+    'assets/img/happy.png',
+    'assets/img/normal.png',
+    'assets/img/bad.png',
+    'assets/img/angry.png',
+  ];
+
+  List<String> status = ['좋음', '보통', '나쁨', '매우나쁨'];
+  String stationName = '중구';
+  List<Mise> data = [];
+
+  int getStatus(Mise mise) {
+    if (mise.pm10! > 150) {
+      return 3;
+    } else if (mise.pm10! > 80) {
+      return 2;
+    } else if (mise.pm10! > 30) {
+      return 1;
+    }
+    return 0;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getSample();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: const Center(child: Text('')),
+      body: getPage(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          MiseApi api = MiseApi();
-          List<Mise> data = await api.getMiseData('중구');
-          data.removeWhere((m) => m.pm10 == 0); // clean the data
-        },
+        onPressed: () async {},
       ),
     );
+  }
+
+  Widget getPage() {
+    if (data.isEmpty) {
+      return Container();
+    }
+
+    int _status = getStatus(data.first);
+
+    return Container(
+      color: colors[_status],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(height: 60),
+          const Text(
+            '현재 위치',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Container(height: 12),
+          Text(
+            '[$stationName]',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16, color: Colors.white),
+          ),
+          Container(height: 60),
+          SizedBox(
+            height: 200,
+            width: 200,
+            child: Image.asset(icon[_status], fit: BoxFit.contain),
+          ),
+          Container(height: 60),
+          Text(
+            status[_status],
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Container(height: 20),
+          Text(
+            '통합 환경 대기 지수 : ${data.first.khai}',
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void getSample() async {
+    MiseApi api = MiseApi();
+    data = await api.getMiseData(stationName);
+    data.removeWhere((m) => m.pm10 == 0); // clean the data
+    setState(() {});
   }
 }
